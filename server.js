1e608247a04ead08c2abece890993eb5f2cc92d9
tnk976 <badrane976@gmail.com>
2018-06-19 11:08:03 +0400

    initialisation du server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+app.listen(3013, function(){
+    console.log('ok')
+})
